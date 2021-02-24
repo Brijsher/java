fb6914b12e97cda7c80d2bb24e30340ce11fb34a
Brijsher <53751958+Brijsher@users.noreply.github.com>
2021-02-25 01:15:13 +0530

    Add files via upload

---
+++ Even_Odd.java
@@ -0,0 +1,15 @@
+public class Even_Odd 
+{
+	public static void main(String[] args) 
+	{
+		int x=21;
+		if(x%2==0)
+		{
+			System.out.println("Number is Even");
+		}
+		else
+		{
+			System.out.println("Number is odd");
+		}
+	}
+}
